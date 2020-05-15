cac484ed10a842e7ed5dd8e4f1f17ae030a3b68d
roufspice <daeyoun.ha27@gmail.com>
2020-05-15 17:14:07 +0900

    2020-05-15-v1

--- src/java_20200515/aa/Notice.java
+++ src/java_20200515/aa/Notice.java
@@ -1,6 +1,9 @@
 package java_20200515.aa;
 
+//클래스의 접근한정자는(Access modifier)는 public, default
 public class Notice {
+	
+	//멤버변수의 접근한정자는(Access modifier)는 public, protected, default, private
 	public int number;
 	protected String title;
 	int hit;
