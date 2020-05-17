20a9dae79b503a1ec60c21410be053582a29e873
게스트1 <1@DESKTOP-ADKP4VR>
2020-05-17 21:41:09 +0900

    2020-05-17-v1

--- src/java_class/BankAccount.java
+++ src/java_class/BankAccount.java
@@ -2,58 +2,77 @@ package java_class;
 
 public class BankAccount {
 	private long balance;
+	//계좌의 주인은 어떻게 표현할 수 있을까요?
 	private Person owner;
-
-	public void setBalance(long pSetBalance) {
-
-		balance = pSetBalance;
-
-	}
-
-	public long getSetBalance() {
+	// Person class를 자료형으로 가진 맴버변수 owner 사용하면된다.
+	//Person 인스턴스를 참조하기 위한 변수 owner를 선언!
+	
+	public long getBalance() {
 		return balance;
 	}
-
-	public void setOwner(Person pOwner) {
-		owner = pOwner;
-
+	
+	public void setBalance(long pBalance) {
+		if(pBalance >= 0) {
+			balance = pBalance;
+		}
 	}
-
+	
 	public Person getOwner() {
 		return owner;
-
 	}
-
-	// parameter: deposit(data type; long)
-	// return (data type; long)
+	public void setOwner(Person pOwner) {
+		owner = pOwner;
+	}
+	
+	//입금기능과 출금, 이체기능이 있어야 한다.
+	//파라미터: 입금할 액수
+	//리턴값으로: 성공여부(boolean)
+	
 	boolean deposit(long amount) {
-		if (amount < 0 || amount > owner.getCashAmount()) {
-			System.out.printf("입금실패입니다. 현재잔액: %d, 현금: %d%n", balance, owner.getCashAmount());
-			return false;
-
-		} else {
-			
-			
-			balance += amount;
+	//리턴타입 메소드이름(파라미터값)
+		if(amount >= 0 && amount <= owner.getCashAmount()) {  //owner.cashAmount = Person owner = Person(); 
+			//owner.cashAmount -= amount; //은행계좌 주인의 현금보유액은 입금한 금액만큼 감소한다는 의미!
 			owner.setCashAmount(owner.getCashAmount() - amount);
-			System.out.printf("%d 원 입금완료! 현재잔액: %d, 현금: %d%n", amount, balance, owner.getCashAmount());
+			balance += amount;
+			System.out.printf("%d원 입금하였습니다. 잔고: %d원, 현금: %d원%n", amount, balance, owner.getCashAmount());
+			System.out.println("true");
+			
 			return true;
-		}
-
-	}
-
-	boolean withdraw(long amount) {
-		if (amount < 0 || amount > balance) {
-			System.out.printf("출금실패입니다. 잔액이 모자랍니다. 현재잔액: %d, 현금: %d%n", balance, owner.getCashAmount());
-			return false;
-
+			
 		} else {
-			owner.cashAmount += amount;
+			System.out.printf("입금 실패입니다. 잔고: %d원, 현금: %d원%n", balance, owner.getCashAmount());
+			System.out.println("false");
+			return false;
+		}
+		
+	}
+	
+	//파라미터: 출금할 액수
+	//리턴값: 성공여부(boolean)
+	
+	boolean withdraw(long amount) {
+		if(amount >= 0 && amount <= balance) {
+//			owner.cashAmount += amount;
+			owner.setCashAmount(owner.getCashAmount() + amount);
 			balance -= amount;
-			System.out.printf("%d 원 출금완료! 현재잔액: %d, 현금: %d%n", amount, balance, owner.getCashAmount());
+			System.out.printf("%d원 출금하였습니다. 잔고: %d원, 현금: %d원%n", amount, balance, owner.getCashAmount());
+			System.out.println("true");
 			return true;
+		} else {
+			System.out.printf("출금 실패입니다. 잔고: %d원, 현금: %d원%n", balance, owner.getCashAmount());
+			System.out.println("false");
+			return false;
 		}
 
+		
 	}
 
+	
+	//파라미터: 받는사람(person), 이체할금액(long)
+	//리턴값: 성공여부(boolean)
+//	
+//	boolean transfer(Person to, long amount) {
+//		
+//		
+//	}
 }
--- src/java_class/Person.java
+++ src/java_class/Person.java
@@ -3,46 +3,54 @@ package java_class;
 public class Person {
 	private String name;
 	private int age;
-	private int cashAmount;
+	private long cashAmount;
+	//소유계좌는 일단 보류
+	//BankAccount클래스 타입의 참조변수 account를 선언한다. 메모리에 참조변수 account를 위한 공간이 마련된다. 
 	private BankAccount account;
-
-	public void setName(String newName) {
-		name = newName;
-	}
-
-	public String getName() {
-		return name;
-
-	}
-
+	
+	//class밖에서도 접근가능한 메소드를 만들어서 해결해볼게요!
+	//새로운값을 지정하는 메소드 / 읽어오는 메소드를 지정해볼게요
+	
+	//값을 지정하는 메소드
+	//모든class에서 접근가능한 public 메소드,
 	public void setAge(int pAge) {
-		if (pAge > 0) {
-			age = pAge;
-
+		if(pAge >= 0) {
+			age = pAge; 
 		}
 	}
-
+	// 값을 출력하는 메소드를 만듭니다.
 	public int getAge() {
 		return age;
-
 	}
-
-	public void setCashAmount(int pCashAmount) {
+	
+	public void setName(String pName) {
+		name = pName ;
+		
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	
+	public void setCashAmount(long pCashAmount) {
+		if(pCashAmount >= 0) {
 			cashAmount = pCashAmount;
+		}
 	}
-
-	int getCashAmount() {
+	public long getCashAmount() {
 		return cashAmount;
-
+		
 	}
-
 	public void setAccount(BankAccount pAccount) {
 		account = pAccount;
 	}
-
-	public BankAccount getAccount() {
+	
+	public BankAccount getBankAccount() {
 		return account;
-
 	}
+	
 
 }
