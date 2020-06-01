2d53bcb386fcad2cbfabf742954c3614af0e9e78
roufspice <daeyoun.ha27@gmail.com>
2020-06-01 18:26:55 +0900

    2020-06-01-v1

--- src/java_20200513/WhileDemo.java
+++ src/java_20200513/WhileDemo.java
@@ -3,17 +3,17 @@ package java_20200513;
 public class WhileDemo {
 	public static void main(String[] args) {
 		// 1부터 10까지 합을 while문으로 만들어라
-		int i = 1;
-		int sum = 0;
-		while (i <= 10) {
-			sum += i;
-			i++;
-
-		}
-		System.out.println(sum);
+//		int i = 1;
+//		int sum = 0;
+//		while (i <= 10) {
+//			sum += i;
+//			i++;
+//
+//		}
+//		System.out.println(sum);
 
 		// 구구단을 while 문으로 작성해보자!!!
-		i = 2;
+		int i = 2;
 
 		
 		while (i <= 9) {
